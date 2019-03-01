7fa4b8bec2a68aef0297b10462b028ce18fb176d
Prafullata Sonawane <prafullatass@gmail.com>
2019-02-28 21:13:21 -0600

    added buttons to employee card

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -5,18 +5,19 @@ import "./Employee.css"
 class EmployeeList extends Component {
     render() {
         return (
-            <section className="topMargin">
+            <section className="topMargin card-holder">
                 {
                     this.props.employees.map(employee =>
                         <div key={`employee--${employee.id}`} className="card">
                             <div className="card-header bg-info">{employee.designation}</div>
                             <div className="card-body"><strong>{employee.name}</strong></div>
-                            <button className="btn btn-primary">Fire</button>
-                            <button className="btn btn-primary">Edit</button>
-
+                            <div className="btn-container">
+                                <button className="btn btn-outline-danger">Fire</button>
+                                <button className="btn btn-outline-primary">Edit</button>
+                            </div>
                         </div>
                     )}
-
+            <button className="btn btn-danger">Fire</button>
             </section>
         )
     }
